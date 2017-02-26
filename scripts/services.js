743bc21e28a07a50542983d8333346c15391e1d9
Marcus Olsson <ninjasinventedphysics@gmail.com>
2017-02-26 14:12:20 +0100

    Update services.js

--- scripts/services.js
+++ scripts/services.js
@@ -2,7 +2,7 @@ var app = angular.module("app");
 
 app.factory("BackendService", function($cookieStore) {
     var backends = [{
-        host: 'http://dddsample.marcusoncode.se',
+        host: 'https://dddsample.marcusoncode.se',
         description: 'Go (Marcus Olsson)'
     },
         {
